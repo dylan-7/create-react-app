28bf49783ad146dd1441e4be78d88fe3efce230e
dylan-7 <wuxiewang@gmail.com>
2018-02-13 22:06:26 +0800

    :wrench: Add package.json.scripts

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -39,6 +39,7 @@ module.exports = function(
   appPackage.scripts = {
     start: 'react-scripts start',
     build: 'react-scripts build',
+    'build:serve': 'serve public',
     test: 'react-scripts test --env=jsdom',
     eject: 'react-scripts eject',
   };
