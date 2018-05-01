dac2beeb5cdda03ee1ad6f3bf39655fc08c86dfa
dylan-7 <wuxiewang@gmail.com>
2018-05-01 14:00:07 +0800

    upgrade dev

--- packages/react-scripts/scripts/utils/createLintStagedConfig.js
+++ packages/react-scripts/scripts/utils/createLintStagedConfig.js
@@ -5,8 +5,8 @@
 module.exports = (resolve, rootDir, isEjecting) => {
   const config = {
     "src/**/*.{ts,tsx,js,jsx}": [
+      "prettier --print-width 100 --single-quote --trailing-commas all --parser typescript --write",
       "tslint --fix",
-      "prettier --print-width 120 --single-quote --trailing-commas all --parser typescript --write",
       "git add"
     ],
     "yarn.lock": [
